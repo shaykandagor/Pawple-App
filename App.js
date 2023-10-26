d1ce2b4cc8bf408f40cc3cba53f07bcaf06ef6d2
shaykandagor <shaynekandagor@gmail.com>
2023-10-26 18:16:20 +0300

    Home screen wip

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import AccountVerifyScreen from "./app/screens/AccountVerifyScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-import PetInfoScreen from "./app/screens/PetInfoScreen";
-import PetRegisterScreen from "./app/screens/PetRegisterScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import LoginScreen from "./app/screens/LoginScreen";
+import AccountVerifyScreen from "./app/screens/AccountVerifyScreen";
+import PetRegisterScreen from "./app/screens/PetRegisterScreen";
+import PetInfoScreen from "./app/screens/PetInfoScreen";
+import HomeScreen from "./app/screens/HomeScreen";
 
 const App = () => {
-  return <WelcomeScreen />;
+  return <HomeScreen />;
 };
 
 export default App;
